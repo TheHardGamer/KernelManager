2e3370c1fab99712f2f98b047e686eaf2157cefc
TheHardGamer <gamelovr695@gmail.com>
2022-04-04 13:12:03 +0530

    Add more details to home page

--- app/src/main/java/com/varun/kernelmanager/MainActivity.java
+++ app/src/main/java/com/varun/kernelmanager/MainActivity.java
@@ -2,42 +2,59 @@ package com.varun.kernelmanager;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.InputMismatchException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        BottomNavigationView bottomNavigationView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.bottomnav);
         getkernelver();
         getcurrcpugov();
     }
 
     protected void getkernelver() {
         TextView kernelver = findViewById(R.id.kernelversion);
+        TextView numcpu = findViewById(R.id.cpunum);
+        int num = Runtime.getRuntime().availableProcessors();
+        numcpu.setText("Number of CPU Cores: " + String.valueOf(num));
+        int flag = 0;
         try {
             Process root = Runtime.getRuntime().exec("su");
         } catch (IOException e) {
+            flag = 1;
             System.out.println("Failed");
             e.printStackTrace();
         }
         String CAT_KERNEL = "/system/bin/cat /proc/version";
         Process process = null;
-        try {
-            process = Runtime.getRuntime().exec(CAT_KERNEL);
-        } catch (IOException e) {
-            System.out.println("Failed to cat proc");
-            e.printStackTrace();
+        if(flag == 1) {
+            try {
+                process = Runtime.getRuntime().exec("/system/bin/uname -rv");
+            } catch (IOException e) {
+                System.out.println("Failed to uname");
+                e.printStackTrace();
+            }
+        }
+        else {
+            try {
+                process = Runtime.getRuntime().exec(CAT_KERNEL);
+            } catch (IOException e) {
+                System.out.println("Failed to cat proc");
+                e.printStackTrace();
+            }
         }
         assert process != null;
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -53,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             output.append(buffer, 0, read);
         }
         kernelver.setText(output);
+        System.out.println(Runtime.getRuntime().availableProcessors());
     }
 
     @SuppressLint("SetTextI18n")
@@ -78,6 +96,6 @@ public class MainActivity extends AppCompatActivity {
             }
             output.append(buffer, 0, read);
         }
-        cpu0.setText(new StringBuilder().append("CPU0 Governor: ").append(output).toString());
+        cpu0.setText(new StringBuilder().append("CPU0 Cluster: ").append(output).toString());
     }
 }
